027372f8e1fef6f2ab2a2daa253d402dc425d141
cecily-916 <cecily.marroquin@gmail.com>
2022-02-15 20:09:25 -0500

    fixed image upload issue

--- src/components/new-recipe-input/image_upload.js
+++ src/components/new-recipe-input/image_upload.js
@@ -12,7 +12,7 @@ const UploadAndDisplayImage = ({ setImage, setImageDelete, setImageId }) => {
       // let url = URL.createObjectURL(selectedImage);
       const config = {
         headers: {
-          Authorization: "Client-ID 15bebad96249efe",
+          Authorization: `Client-ID ${process.env.CLIENT_ID}`,
         },
       };
 
@@ -44,22 +44,14 @@ const UploadAndDisplayImage = ({ setImage, setImageDelete, setImageId }) => {
           <button onClick={() => setSelectedImage(null)}>Remove</button>
         </div>
       )} */}
-      {selectedImage ? (
-        <p className="italic text-slate-500">
-          Image file: {selectedImage.name}
-        </p>
-      ) : (
-        <input
-          type="file"
-          className="rounded-sm disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-        "
-          name="myImage"
-          onChange={(event) => {
-            console.log(event.target.files[0]);
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
-      )}
+      <input
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
+        }}
+      />
     </div>
   );
 };
